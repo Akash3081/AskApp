8aa1a612deae26100c884a8fbed3efd435850b8e
Akash3081 <akashgupta@blubirch.com>
2023-01-27 11:12:33 +0530

    routes updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :inboxes
   root 'static_pages#landing_page'
+  resources :inboxes
   get 'pricing', to: 'static_pages#pricing'
   get 'privacy', to: 'static_pages#privacy'
   get 'terms', to: 'static_pages#terms'
